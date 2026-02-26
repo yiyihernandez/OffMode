e969c7dc03960997988bd620653d796be78ff566
yiyi hernandez <jirethsuarezhernandez2007@gmail.com>
2026-02-25 19:05:01 -0500

    un poco de detalles

--- js/interaccion.js
+++ js/interaccion.js
@@ -1,13 +1,9 @@
 function mostrarPestaña(idPestaña) {
-    // Seleccionamos todas las secciones con la clase pestaña
     const secciones = document.querySelectorAll('.pestaña');
-    
-    // Las ocultamos todas
     secciones.forEach(seccion => {
         seccion.classList.remove('active');
     });
 
-    // Mostramos solo la que clickeamos
     const pestañaActiva = document.getElementById(idPestaña);
     pestañaActiva.classList.add('active');
 }
